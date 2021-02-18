fb9a40f682bf0da82e19e68826377fc382f8c43e
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-02-18 11:46:51 +0530

    some clean up

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,10 @@ fn main() {
     let mut path = PathBuf::new();
     path.push(arg);
     let list_of_dirs = list_dirs(path);
-    //for i in list_of_dirs.iter() {
-    //    //view_file_contents(PathBuf::from(i));
-    //    let content = read_to_string(i);
-    //    println!("{:?}", content);
-    //}
-    //error
-    //list_of_dirs.iter().filter_map(|x| read_to_string(x));
-    //playing_with_paths();
+    let list_of_dirs: Vec<_> = list_of_dirs.iter().filter_map(|x| read_to_string(x).ok()).collect();
+    for i in list_of_dirs.iter() {
+        println!("{}", i);
+    }
 }
 
 // like a ls clone
@@ -24,7 +20,6 @@ fn list_dirs(path: PathBuf) -> Vec<PathBuf> {
         .expect("failed to read dirs")
         .map(|x| x.unwrap().path())
         .collect();
-    println!("{:?}", path);
     return path;
 }
 
@@ -33,15 +28,6 @@ fn view_file_contents(path: PathBuf) {
     let content = read_to_string(path)
         .expect("failed to read file");
     println!("{}", content);
-}
-
-fn playing_with_paths() {
-    let mut path = PathBuf::new();
-    let path2 = PathBuf::from("password");
-    path.push("/");
-    path.push("etc");
-    let path = path.join(path2);
-    println!("{:?}", path.into_os_string().into_string().unwrap());
 }
 
 // recursively list dirs
