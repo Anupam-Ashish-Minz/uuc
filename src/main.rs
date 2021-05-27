28af05e438d4fe0239f2799c58fe04d55c1aec4f
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-05-27 21:26:51 +0530

    some formatting

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,17 @@ use file_handler::format_file_list;
 
 fn main() {
     let path = parse_options();
-    if let Some(path) = path {
-        let file_list = match list_files(path) {
-            Ok(path) => path,
-            Err(e) => { println!("{}", e); exit(1); }
-        };
-        let file_list = format_file_list(file_list);
 
-        println!("{}", file_list);
-    } else {
-        println!("please enter a file path");
-        exit(1);
-    }
+    let path = match path {
+        Some(path) => path,
+        None => { println!("enter a file path"); exit(1); }
+    };
+
+    let file_list = match list_files(path) {
+        Ok(path) => path,
+        Err(e) => { println!("{}", e); exit(1); }
+    };
+    let file_list = format_file_list(file_list);
+
+    println!("{}", file_list);
 }
