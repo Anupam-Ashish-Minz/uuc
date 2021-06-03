4fea2e5a1bcbd20da6a13c48bc2476f2880ce136
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-06-03 20:15:49 +0530

    when no directory is specified the files in the current are printed

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 use std::process::exit;
+use std::path::PathBuf;
 
 mod cli;
 mod file_handler;
 
 use cli::parse_options;
-use file_handler::list_files;
+use file_handler::get_list_of_files;
 use file_handler::format_file_list;
 
 fn main() {
+    list_files();
+}
+
+fn list_files() {
     let path = parse_options();
 
     let path = match path {
         Some(path) => path,
-        None => { println!("enter a file path"); exit(1); }
+        None => PathBuf::from(".")
     };
 
-    let file_list = match list_files(path) {
+    let file_list = match get_list_of_files(path) {
         Ok(path) => path,
         Err(e) => { println!("{}", e); exit(1); }
     };
